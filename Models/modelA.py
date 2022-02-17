adea7380bbb5cf55b1cf07ba809bc667e299aae7
deeplearning2020 <yeahia.ruet@gmail.com>
2022-02-17 23:38:39 +0600

    sdfsdf|

--- Models/modelA.py
+++ Models/modelA.py
@@ -36,7 +36,7 @@ class CoordAtt(nn.Module):
         #mip = max(8, inp // reduction)
 
         self.conv1 = nn.Conv2d(inp, inp//2, kernel_size=1, stride=1, padding=0)
-        self.bn1 = nn.BatchNorm2d(oup)
+        self.bn1 = nn.BatchNorm2d(inp//2)
         self.act = h_swish()
         
         self.conv_h = nn.Conv2d(inp//2, oup, kernel_size=1, stride=1, padding=0)
