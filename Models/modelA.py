6a6a137883f152d11589a3ab44cd9b0f36fe575a
deeplearning2020 <yeahia.ruet@gmail.com>
2022-02-17 21:19:16 +0600

    dsfasd

--- Models/modelA.py
+++ Models/modelA.py
@@ -81,7 +81,7 @@ class modelA(nn.Module):
         self.bn2 = nn.BatchNorm2d(32)
         self.conv2 = nn.Conv2d(32, 16, kernel_size=3, stride=1, bias=False)
         self.bn3 = nn.BatchNorm2d(16)
-        self.conv3 = nn.Conv2d(16, 8, kernel_size=3, stride=1, bias=False)
+        self.conv3 = nn.Conv2d(16, 16, kernel_size=3, stride=1, bias=False)
         sz = 2*self.patch_size + 1 - 2*(3-1)
 
         self.bn4 = nn.BatchNorm1d(16*sz*sz)
