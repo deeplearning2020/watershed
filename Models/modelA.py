e7bc42706e579fcf20fcdd71ab62b108888da5bf
deeplearning2020 <yeahia.ruet@gmail.com>
2022-02-19 13:16:31 +0600

    dsfsdf

--- Models/modelA.py
+++ Models/modelA.py
@@ -67,6 +67,43 @@ class CoordAtt(nn.Module):
         out = identity * a_w * a_h
 
         return out
+        
+class SpatialGCN(nn.Module):
+    def __init__(self, plane):
+        super(SpatialGCN, self).__init__()
+        inter_plane = plane // 2
+        self.node_k = nn.Conv2d(plane, inter_plane, kernel_size=1)
+        self.node_v = nn.Conv2d(plane, inter_plane, kernel_size=1)
+        self.node_q = nn.Conv2d(plane, inter_plane, kernel_size=1)
+
+        self.conv_wg = nn.Conv1d(inter_plane, inter_plane, kernel_size=1, bias=False)
+        self.bn_wg = BatchNorm1d(inter_plane)
+        self.softmax = nn.Softmax(dim=2)
+
+        self.out = nn.Sequential(nn.Conv2d(inter_plane, plane, kernel_size=1),
+                                 BatchNorm2d(plane))
+
+    def forward(self, x):
+        # b, c, h, w = x.size()
+        node_k = self.node_k(x)
+        node_v = self.node_v(x)
+        node_q = self.node_q(x)
+        b,c,h,w = node_k.size()
+        node_k = node_k.view(b, c, -1).permute(0, 2, 1)
+        node_q = node_q.view(b, c, -1)
+        node_v = node_v.view(b, c, -1).permute(0, 2, 1)
+        # A = k * q
+        # AV = k * q * v
+        # AVW = k *(q *v) * w
+        AV = torch.bmm(node_q,node_v)
+        AV = self.softmax(AV)
+        AV = torch.bmm(node_k, AV)
+        AV = AV.transpose(1, 2).contiguous()
+        AVW = self.conv_wg(AV)
+        AVW = self.bn_wg(AVW)
+        AVW = AVW.view(b, c, h, -1)
+        out = F.relu_(self.out(AVW) + x)
+        return out
 
 class DualGCN(nn.Module):
     """
