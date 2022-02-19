ae2efc7d02d8e4135acaa44c4dd9b29c43fcb0b3
deeplearning2020 <yeahia.ruet@gmail.com>
2022-02-19 14:46:17 +0600

    lets hope for the best

--- Models/modelA.py
+++ Models/modelA.py
@@ -225,7 +225,7 @@ class modelA(nn.Module):
 
     def forward_rep(self, x):
         out = F.dropout(F.relu(self.graph1(self.cord1(self.conv1(self.bn1(x))))),p=0.2)
-        out = F.fropout(F.relu(self.graph2(self.cord2(self.conv2(self.bn2(out))))), p=0.2)
+        out = F.dropout(F.relu(self.graph2(self.cord2(self.conv2(self.bn2(out))))), p=0.2)
         out = F.relu(self.conv3(self.bn3(out)))
         #out = F.relu(self.cord1(out))
         out = torch.reshape(out, (len(out), -1))
